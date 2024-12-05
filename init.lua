4647ed0134a1f2f60133ae4da890003112ecdcb6
freskey <frowkzey@gmail.com>
2024-12-05 22:38:42 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("config.core")
+require("config.remap")
+require("config.lazy")
---
+++ lua/config/core.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.ignorecase = true
+
+vim.opt.smartindent = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.termguicolors = true
